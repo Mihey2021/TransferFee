4414b8babe5acb82f5c75a2e433de07bd701d4e8
Mihey2021 <hunter_m@list.ru>
2022-03-26 11:27:05 +0300

    Initial commit

---
+++ src/main/kotlin/TransferFee.kt
@@ -0,0 +1,11 @@
+const val PERCENT = 0.0075
+const val TOTAL_IN_ROUBLE = 100.0
+const val MIN_TAX = 35.0 * TOTAL_IN_ROUBLE
+
+fun main() {
+    print("Введите сумму перевода: ")
+    val amount = readln().toInt() * TOTAL_IN_ROUBLE;
+    val commission = if (amount <= MIN_TAX) MIN_TAX else amount * PERCENT
+    val commissionRub: Double = commission / TOTAL_IN_ROUBLE
+    println("Комиссия за перевод составит $commissionRub руб.")
+}
